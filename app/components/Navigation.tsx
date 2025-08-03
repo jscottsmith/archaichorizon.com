6ab0a10941189756285e405bfbc374a202396de4
J Scott Smith <jscsmith@gmail.com>
2025-08-02 22:56:57 -0700

    feat: theme switcher

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuTrigger,
 } from "../../components/ui/dropdown-menu";
 import { Button } from "./ui/button";
+import { ThemeSwitcher } from "../../components/ui/theme-switcher";
 
 export function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,31 +20,34 @@ export function Navigation() {
   };
 
   return (
-    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="h-8 w-8">
-          <Menu className="h-4 w-4" />
-          <span className="sr-only">Toggle navigation menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-48">
-        <DropdownMenuItem asChild>
-          <Link href="/" className="flex items-center" onClick={closeMenu}>
-            <Home className="mr-2 h-4 w-4" />
-            <span>Home</span>
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link
-            href="/collection"
-            className="flex items-center"
-            onClick={closeMenu}
-          >
-            <Disc3 className="mr-2 h-4 w-4" />
-            <span>Collection</span>
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex items-center gap-2">
+      <ThemeSwitcher />
+      <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon" className="h-8 w-8">
+            <Menu className="h-4 w-4" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem asChild>
+            <Link href="/" className="flex items-center" onClick={closeMenu}>
+              <Home className="mr-2 h-4 w-4" />
+              <span>Home</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link
+              href="/collection"
+              className="flex items-center"
+              onClick={closeMenu}
+            >
+              <Disc3 className="mr-2 h-4 w-4" />
+              <span>Collection</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
