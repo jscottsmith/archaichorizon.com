6e90455d1e512725dc715f42ff63349ded9e941d
J Scott Smith <jscsmith@gmail.com>
2025-08-08 13:57:24 -0700

    feat: logotype

--- app/(interface)/page.tsx
+++ app/(interface)/page.tsx
@@ -1,7 +1,9 @@
+import { Logotype } from "../components/Logo";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1>AH Home</h1>
+    <div className="flex items-center justify-center h-svh">
+      <Logotype className="w-64" />
     </div>
   );
 }
