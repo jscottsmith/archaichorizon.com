ddab71a47b9cb0aaa990dea01233cfeff8d51936
J Scott Smith <jscsmith@gmail.com>
2025-08-29 12:06:52 -0700

    fix: image transformations cache min

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://archive.org/**")],
+    remotePatterns: [new URL("https://archive.org/serve/**")],
+    minimumCacheTTL: 31536000, // 1 year
   },
 };
 
