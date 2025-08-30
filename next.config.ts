ce143ed7a9e3d689d7d8fec1d1f5cc4fdd180dc9
J Scott Smith <jscsmith@gmail.com>
2025-08-29 23:19:28 -0700

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://archive.org/serve/**")],
+    remotePatterns: [
+      new URL("https://archive.org/serve/**"),
+      new URL("https://archive.org/services/img/**"),
+    ],
     minimumCacheTTL: 31536000, // 1 year
   },
 };
